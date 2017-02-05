2233d2141128fe5a3defd3acf53af30bab0c0f4e
Ernesto Carrea <equistango@gmail.com>
2017-02-04 23:07:57 -0300

    Versión

--- Base/Properties/AssemblyInfo.cs
+++ Base/Properties/AssemblyInfo.cs
@@ -7,37 +7,15 @@ using System.Runtime.InteropServices;
 
 #endregion
 
-// La información general de un ensamblado se controla mediante el siguiente 
-// conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada con un ensamblado.
 [assembly: AssemblyTitle("Lázaro Base")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyDescription("Lázaro Base versión 1.0")]
 [assembly: AssemblyCompany("Ernesto Nicolás Carrea")]
 [assembly: AssemblyProduct("Lázaro")]
 [assembly: AssemblyCopyright("Copyright 2004-2017 Ernesto Nicolás Carrea y colaboradores")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
-// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
-// COM, establezca el atributo ComVisible en true en este tipo.
-[assembly: ComVisible(false)]
-
-// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("c47fcc4e-7d7e-4b81-acf8-af83b0997566")]
-
-// La información de versión de un ensamblado consta de los cuatro valores siguientes:
-//
-//      Versión principal
-//      Versión secundaria 
-//      Número de compilación
-//      Revisión
-//
-// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
-// mediante el carácter '*', como se muestra a continuación:
-// [assembly: AssemblyVersion("2.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: CLSCompliant(true)]
+[assembly: Guid("c47fcc4e-7d7e-4b81-acf8-af83b0997566")]
 [assembly: NeutralResourcesLanguageAttribute("es-AR")]
