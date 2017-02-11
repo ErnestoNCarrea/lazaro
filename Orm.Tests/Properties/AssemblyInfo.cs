ad15d22254601dd2d1538173827c3c5adde65cda
Ernesto Carrea <equistango@gmail.com>
2017-02-11 13:00:11 -0300

    Proyectos, referencias, configuraciones Release y Debug

---
+++ Orm.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+using System.Security.Permissions;
+
+#endregion
+
+[assembly: AssemblyTitle("Lázaro ORM Tests")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyDescription("Lázaro ORM version 1.0")]
+[assembly: AssemblyCompany("Ernesto Nicolás Carrea")]
+[assembly: AssemblyProduct("Lázaro")]
+[assembly: AssemblyCopyright("Copyright 2017 Ernesto Nicolás Carrea")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+[assembly: Guid("d29c6652-266a-41d4-b392-65e6a935f920")]
